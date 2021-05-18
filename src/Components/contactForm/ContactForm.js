e65dd5e890c631f915abe54d9db8df66d8428a95
Norrismi <Norrismi0@gmail.com>
2021-05-18 10:57:31 -0400

    form working reworking for validation

--- src/Components/contactForm/ContactForm.js
+++ src/Components/contactForm/ContactForm.js
@@ -1,123 +1,134 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import {db} from '../../firebase'
-import {useAlert} from 'react-alert'
+import { useAlert } from "react-alert";
+import './contactForm.css'
 
 
-let emailRegex =  RegExp(/[^@]+@[^.]+\..+/);
 
 
-class ContactForm extends Component {
-
-    state = {
-        name: '',
-        phone: '',
-        email:'',
-        message: '',
-        formErrors: []
-    }
+const ContactForm = () =>  {
 
 
-    handleChange = event => {
-        event.preventDefault();
-        const {name, value} = event.target;
-        let formErrors = this.state.formErrors;
+    const [name, setName] = useState("")
+    const [phone, setPhone] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
 
-        // switch(name){
-        //     case "email":
-        //         formErrors.email = emailRegex.test(value)
-        //             ? null  
-        //             : "Please enater a valid email address"
-        //         break;
+    // const handleChange = event => {
+    //     event.preventDefault();
+    //     const {name, value} = event.target;
+    //     let formErrors = this.state.formErrors;
+
+    //     // switch(name){
+    //     //     case "email":
+    //     //         formErrors.email = emailRegex.test(value)
+    //     //             ? null  
+    //     //             : "Please enater a valid email address"
+    //     //         break;
             
-        //         default:
-        //             break;
-        // }
-        // this.setState({ formErrors, [name]:value })
-    }
+    //     //         default:
+    //     //             break;
+    //     // }
+    //     // this.setState({ formErrors, [name]:value })
+    // }
 
-    handleSubmit = event => {
+    const handleSubmit = event => {
         event.preventDefault()
-        const {name, phone, email, message} = this.state
+        // let message =()=> {
 
-        if(email){
-          db.collection('submissions').add({
-              name: name,
-              phone: phone,
-              email: email,
-              message: message
-          })
-          .then(() => {
-            useAlert.success("Message submitted successfully!");
-          })
-        //   .catch((error)=>{
-        //     useAlert.error("Please make sure all fields are completed!");
-        //   })
-
+            if(email){
+                db.collection('submissions').add({
+                    name: name,
+                    phone: phone,
+                    email: email,
+                    message: message
+                })
+                // .then(() => {
+                //     useAlert.success("Message submitted successfully!");
+                // })
+                // .catch((error)=>{
+                //     alert(error.message);
+                // })
+                
+                setName('')
+                setPhone('')
+                setEmail('')
+                setMessage('')
+                
+            }
  
-        }
+        // }
     }
 
 
 
 
 
-    render() {
-       
-        let {formErrors} = this.state
-        console.log(this.state)
+
+        const alert = useAlert();
+        // let {formErrors} = this.state
+  
         return (
             <div className="contact-form_container">
                 <h4 className="contact-form_title">Send a Comment or Question </h4>
-                    <div className="contact-form_subtitle">Send a message to the owner's Beckey and Mike Norris</div>
-                <form className="contact-form" onSubmit={this.handleSubmit}>
+                    {/* <div className="contact-form_subtitle">Send a message to the owners</div> */}
+                <form className="contact-form" 
+                 onSubmit={handleSubmit}
+                >
                     <div className="form-name">
                         <input
                             type="text"
                             name="name"
-                            value={this.state.name}
-                            onChange={this.handleChange}
-                            placeholder="Fist and Last Name"
+                            value={name}
+                            onChange={(e)=> setName(e.target.value)}
+                            placeholder="First and Last Name"
                         />
                     </div>
-                    <br></br>
+                    {/* <br></br> */}
                     <div className="form-phone">
                         <input
                             type="tel"
                             name="phone"
-                            value={this.state.phone}
-                            onChange={this.handleChange}
+                            value={phone}
+                            onChange={(e)=> setPhone(e.target.value)}
                             placeholder="Cell Phone Number"
                         />
                     </div>
                     {/* {formErrors.phone ? <span>{formErrors.phone}</span> : null} */}
-                    <br></br>
+                    {/* <br></br> */}
                     <div className="form-email ">
                         <input
                             type="email"
                             name="email"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                            placeholder="Name@email.com"
+                            value={email}
+                            onChange={(e)=> setEmail(e.target.value)}
+                            placeholder = "Name@email.com"
                             aria-describedby="emailHelp"
-                            // required
+                            required
                         />
                     </div>
                     {/* {formErrors.email ? <span>{formErrors.email}</span> : null} */}
                     <div className="form-message">
                         <textarea
                             name="message"
-                            value={this.state.message}
-                            onChange={this.handleChange}
+                            value={message}
+                            onChange={(e)=> setMessage(e.target.value)}
                             placeholder="I'd like to know more about 15 Jarvis Creek Lane."
-                            rows="20"
+                            rows="5"
                             required
                         ></textarea>
                     </div>
-                    <button className="contact-form_button  btn btn-primary" type="submit">Submit</button>
+                    <button className="contact-form_button " type="submit"
+                             onClick={ 
+                                handleSubmit
+                                //  message()
+                             
+                              }
+                    >Ask a Question</button>
                 </form>
             </div>
         );
-    }
+ 
 }
 
 export default ContactForm;
