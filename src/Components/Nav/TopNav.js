a3c81d2672530731da297e1dd601c1db763c6b09
Norrismi <Norrismi0@gmail.com>
2021-06-12 16:07:35 -0400

    fix nav bar

--- src/Components/Nav/TopNav.js
+++ src/Components/Nav/TopNav.js
@@ -6,16 +6,16 @@ const TopNav = () => {
 
         <div className="navbar  navbar-expand-lg navbar-dark bg-dark sticky-top">
 
-            <button className="navbar-toggler" type="button" data-toggle="collapse"
+            {/* <button className="navbar-toggler" type="button" data-toggle="collapse"
                 data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01"
                 aria-expanded="false" aria-label="Toggle navigation">
 
                 <span className="navbar-toggler-icon"></span>
-            </button>
-            <a className="navbar-brand" href="/">
+            </button> */}
+            <a className="navbar-brand mx-auto" href="/">15 Jarvis Creek Lane
 
                 {/* <img className=" nav-bar_big-logo" src="15 Jarvis Creek" alt='site logo and alternative home button' /> */}
-                <a class="navbar-brand justify-content-center" href="#/">15 Jarvis Creek Ln.</a>
+                {/* <a class="navbar-brand justify-content-center" href="#/">15 Jarvis Creek Ln.</a> */}
 
             </a>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
