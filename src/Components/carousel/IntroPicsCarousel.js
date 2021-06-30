8d19fa94ce13e9db06184a76e62110d6238efba3
Norrismi <Norrismi0@gmail.com>
2021-06-30 13:39:18 -0400

    added house pictures

--- src/Components/carousel/IntroPicsCarousel.js
+++ src/Components/carousel/IntroPicsCarousel.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
-// import one from '../../Assets/carouselPics/one.png'
-// import two from '../../Assets/carouselPics/two.jpg'
-// import three from '../../Assets/carouselPics/three.jpg'
-
 import front from '../../Assets/carouselPics/JarvisCreekP.jpeg'
 import sittingArea from '../../Assets/carouselPics/JarvisCreekS-L.jpeg'
 import pond from '../../Assets/carouselPics/JarvisCreekP-L.jpeg'
@@ -15,15 +11,13 @@ class introPicsCarousel extends Component {
             <Carousel className='carousel'>
                 <div>
                     <img src={front} alt='one' />
-                    {/* <p className="legend">Legend 1</p> */}
                 </div>
                 <div>
                     <img src={sittingArea} alt='two' />
-                    {/* <p className="legend">Legend 2</p> */}
+                    <p className="legend">Sitting area in front of pond</p>
                 </div>
                 <div>
                     <img src={pond} alt='three' />
-                    {/* <p className="legend">Legend 3</p> */}
                 </div>
             </Carousel>
         );
