05bf8c770c9df806e9279abfe74ee61f160d5741
Norrismi <Norrismi0@gmail.com>
2021-06-30 13:36:18 -0400

    added pictures

--- src/Components/carousel/IntroPicsCarousel.js
+++ src/Components/carousel/IntroPicsCarousel.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
-import one from '../../Assets/carouselPics/one.png'
-import two from '../../Assets/carouselPics/two.jpg'
-import three from '../../Assets/carouselPics/three.jpg'
+// import one from '../../Assets/carouselPics/one.png'
+// import two from '../../Assets/carouselPics/two.jpg'
+// import three from '../../Assets/carouselPics/three.jpg'
+
+import front from '../../Assets/carouselPics/JarvisCreekP.jpeg'
+import sittingArea from '../../Assets/carouselPics/JarvisCreekS-L.jpeg'
+import pond from '../../Assets/carouselPics/JarvisCreekP-L.jpeg'
 
 class introPicsCarousel extends Component {
     render() {
         return (
             <Carousel className='carousel'>
                 <div>
-                    <img src={one} alt='one' />
+                    <img src={front} alt='one' />
                     {/* <p className="legend">Legend 1</p> */}
                 </div>
                 <div>
-                    <img src={two} alt='two' />
+                    <img src={sittingArea} alt='two' />
                     {/* <p className="legend">Legend 2</p> */}
                 </div>
                 <div>
-                    <img src={three} alt='three' />
+                    <img src={pond} alt='three' />
                     {/* <p className="legend">Legend 3</p> */}
                 </div>
             </Carousel>
