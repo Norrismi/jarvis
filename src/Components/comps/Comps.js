e49d13e8bbe5ce4aa568e6a7146b4705375cf5b6
Norrismi <Norrismi0@gmail.com>
2022-04-14 14:23:19 -0400

    component comps fixed

--- src/Components/comps/Comps.js
+++ src/Components/comps/Comps.js
@@ -1,70 +1,33 @@
 import React from 'react';
 import './comps.css'
-import jarvis17 from '../../Assets/comps/jarvis17.jpg'
-import jarvis87 from '../../Assets/comps/87Jarvis.jpg'
+import Data from '../../Data'
+
 
 const Comps = () => {
     return (
         <div className="comps_container">
-
             <h4 className="comps_title">Nearby Similar Homes</h4>
 
             <div className="comps_cards-container">
-                <a className="card comps_card" 
-                target="_blank" 
-                rel="noreferrer" 
-                href='https://www.zillow.com/homedetails/17-Jarvis-Creek-Ln-Hilton-Head-Island-SC-29926/2075310809_zpid/'>
-                    <img className="card-img-top" src={jarvis17} alt="Card cap" />
-                    <div className="card-body">
-                        <div className="comps_price">$555,555</div>
-                        <div className="comps_row-container">
+                {Data.map(property => (
+                    <a className="card comps_card"
+                        target="_blank"
+                        rel="noreferrer"
+                        href={property.href}>
+                        <img className="card-img-top" src={property.src} alt="Card cap" />
+                        <div className="card-body">
+                            <div className="comps_price">{property.price}</div>
+                            <div className="comps_row-container">
 
-                            <div className="comps_beds comps_stat">3 Beds</div>
-                            <div className="comps_bath comps_stat">2 Bath</div>
-                            <div className="comps_sqft comps_stat">2,000 Sq Ft.</div>
+                                <div className="comps_beds comps_stat">{property.beds}</div>
+                                <div className="comps_bath comps_stat">{property.baths}</div>
+                                <div className="comps_sqft comps_stat">{property.sqft}</div>
+                            </div>
+                            <div className="comps_address">{property.address}</div>
                         </div>
-                        <div className="comps_address">15 Main Street, Hilton Head, SC 29926</div>
-                    </div>
-                </a>
-
-                <a className="card comps_card" 
-                target="_blank" 
-                rel="noreferrer" 
-                href='https://www.zillow.com/homedetails/87-Jarvis-Creek-Ln-Hilton-Head-Island-SC-29926/98840964_zpid/'>
-                    <img className="card-img-top" src={jarvis87} alt="Card cap" />
-                    <div className="card-body">
-                        <div className="comps_price">$499,700</div>
-                        <div className="comps_row-container">
-
-                            <div className="comps_beds comps_stat">3 Beds</div>
-                            <div className="comps_bath comps_stat">2 Bath</div>
-                            <div className="comps_sqft comps_stat">1,889 Sq Ft.</div>
-                        </div>
-                        <div className="comps_address">87 Jarvis Creek Ln, Hilton Head, SC 29926</div>
-                    </div>
-                </a>
-
-
-                <a className="card comps_card" target="_blank" rel="noreferrer" href='https://www.zillow.com/homedetails/17-Jarvis-Creek-Ln-Hilton-Head-Island-SC-29926/2075310809_zpid/'>
-                    <img className="card-img-top" src={jarvis17} alt="Card cap" />
-                    <div className="card-body">
-                        <div className="comps_price">$555,555</div>
-                        <div className="comps_row-container">
-
-                            <div className="comps_beds comps_stat">3 Beds</div>
-                            <div className="comps_bath comps_stat">2 Bath</div>
-                            <div className="comps_sqft comps_stat">2,000 Sq Ft.</div>
-                        </div>
-                        <div className="comps_address">15 Main Street, Hilton Head, SC 29926</div>
-                    </div>
-                </a>
-
-  
-
-       
+                    </a>
+                ))}
             </div>
-
-
         </div>
     );
 }
---
+++ src/Data.js
@@ -0,0 +1,42 @@
+
+import jarvis17 from './Assets/comps/jarvis17.jpg'
+import jarvis87 from './Assets/comps/87Jarvis.jpg'
+import jarvis28 from './Assets/comps/28JarvisCreekLN.jpg'
+import jarvis58 from './Assets/comps/58JarvisCreekLane.png'
+
+const Data = [
+
+    {
+        id: 1,
+        href: "https://www.zillow.com/homedetails/28-Jarvis-Creek-Ct-Hilton-Head-Island-SC-29926/98844634_zpid/",
+        src: jarvis28,
+        price: '$489,900',
+        beds: "3 Beds" ,
+        baths: "3 Bath" ,
+        sqft: "2,310 Sq Ft.",
+        address: "28 Jarvis Creek Lane, Hilton Head Island, SC 29926" ,
+    },
+    {
+        id: 2,
+        href: "https://www.zillow.com/homedetails/58-Jarvis-Creek-Ln-Hilton-Head-Island-SC-29926/98862550_zpid/",
+        src: jarvis58,
+        price: '$590,000',
+        beds: "3 Beds" ,
+        baths: "3 Bath" ,
+        sqft: "2,029 Sq Ft.",
+        address: "58 Jarvis Creek Lane, Hilton Head Island, SC 29926" ,
+    },
+    {
+        id: 3,
+        href: "https://www.zillow.com/homedetails/17-Jarvis-Creek-Ln-Hilton-Head-Island-SC-29926/2075310809_zpid/",
+        src: jarvis17,
+        price: '$555,555',
+        beds: "3 Beds" ,
+        baths: "3 Bath" ,
+        sqft: "2,310 Sq Ft.",
+        address: "15 Main Street, Hilton Head Island, SC 29926" ,
+    },
+
+]
+
+export default Data;
